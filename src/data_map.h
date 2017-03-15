825ca34b52eb0e535f778f0417c831fe25e0567f
Yunming Zhang <zhangyunming1990@gmail.com>
2017-03-15 11:57:10 -0400

    starting to add support for ligra sparset

--- src/data_map.h
+++ src/data_map.h
@@ -9,6 +9,7 @@
 #include <unordered_map>
 #include "pvector.h"
 #include <google/dense_hash_map>
+#include <ligra/sparseSet.h>
 
 using std::unordered_map;
 
@@ -318,6 +319,105 @@ public:
         return buffer;
     }
 };
+
+template <typename T, typename V>
+class LigraSparseDataMap : public DataMap <T,V> {
+
+private:
+    V init_val_;
+
+    //google::dense_hash_map<T, V> data_map_;
+    sparseAdditiveSet<V> data_map_;
+    int num_updated_;
+
+public:
+    LigraSparseDataMap<T,V> (size_t init_size) : DataMap<T,V> (){
+        num_updated_ = 0;
+    }
+
+    virtual ~LigraSparseDataMap() {
+        //delete data_map_;
+    }
+
+    // set up the Google dense hash
+    virtual void init(V init_val) {
+        init_val_ = init_val;
+    }
+
+    // return the value in Google dense hash
+    virtual V find(T index) {
+        return data_map_[index];
+    }
+
+    // increment if found, otherwise, initialize
+    virtual void find_and_add(T index, V val) {
+//        // Data Map does not contain the entry
+//        if (data_map_.find(index) != data_map_.end()) {
+//            data_map_[index] = data_map_[index] + val;
+//        } else {
+//            data_map_[index] = init_val_ + val;
+//            num_updated_++;
+//        }
+    }
+
+    virtual int num_updated() {
+        return num_updated_;
+    }
+
+    // get keys with top values
+    virtual pvector<T> get_top_keys(int top_count) {
+        pvector<T> buffer(num_updated_);
+        size_t output_count = top_count < num_updated_ ? top_count : num_updated_;
+        pvector<T> output_buffer(output_count);
+
+//        int i = 0;
+//        for (auto kv : data_map_){
+//            buffer[i] = kv.first;
+//            i++;
+//        }
+//
+//        sort(buffer.begin(), buffer.end(), [this] (NodeID const& a, NodeID const& b) {
+//            bool output = FALSE;
+//            if (data_map_[a] > data_map_[b]){
+//                output = TRUE;
+//            } else if (data_map_[a] == data_map_[b]){
+//                if (a > b){
+//                    output = TRUE;
+//                }
+//            }
+//            return output;
+//        });
+//
+//        for (int i = 0; i < output_count; i++){
+//            output_buffer[i] = buffer[i];
+//        }
+        return output_buffer;
+    }
+
+    // get all keys sorted by their values
+    virtual pvector<T> get_updated_keys() {
+        pvector<T> buffer(num_updated_);
+//        int i = 0;
+//        for (auto kv : data_map_){
+//            buffer[i] = kv.first;
+//            i++;
+//        }
+//        sort(buffer.begin(), buffer.end(), [this] (NodeID const& a, NodeID const& b) {
+//            bool output = FALSE;
+//            if (data_map_[a] > data_map_[b]){
+//                output = TRUE;
+//            } else if (data_map_[a] == data_map_[b]){
+//                if (a > b){
+//                    output = TRUE;
+//                }
+//            }
+//            return output;
+//        });
+
+        return buffer;
+    }
+};
+
 
 
 #endif //MIGRA_GAPBS_DATA_MAP_H

281dfc191e3bcfc9a8d52b6c1548684e7b935bf7
zhangyunming1990@gmail.com <zhangyunming1990@gmail.com>
2018-10-10 18:19:18 -0400

    adding kcore

---
+++ src/kcore.cc
@@ -0,0 +1,21 @@
+// Copyright (c) 2015, The Regents of the University of California (Regents)
+// See LICENSE.txt for license details
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include "benchmark.h"
+#include "builder.h"
+#include "command_line.h"
+#include "graph.h"
+#include "pvector.h"
+
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+
+}
